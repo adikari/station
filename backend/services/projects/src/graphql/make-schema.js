e6a576df9461cfe0af81a2093bfd89a589685915
Steven Phan <steven.phan@acloud.guru>
2019-11-01 16:20:37 +1100

    fix bug

--- backend/services/projects/src/graphql/make-schema.js
+++ backend/services/projects/src/graphql/make-schema.js
@@ -1,5 +1,5 @@
 const { makeExecutableSchema } = require('graphql-tools');
-
+const merge = require('lodash.merge');
 const rootSchema = require('./schemas/root');
 const projectSchema = require('./schemas/project');
 
@@ -8,7 +8,7 @@ const makeSchema = () => {
 
   const typeDefs = schemas.reduce((acc, schema) => acc.concat(schema.typeDefs), '');
 
-  const resolvers = schemas.reduce((acc, schema) => Object.assign(acc, schema.resolvers), {});
+  const resolvers = Object.assign(schemas.reduce((acc, schema) => merge(acc, schema.resolvers), {}));
 
   return makeExecutableSchema({
     typeDefs,
