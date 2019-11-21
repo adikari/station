8d085b6ecbd8ae33bdca620203bdf9895b6105a1
subash adhikari <me@subash.com.au>
2019-11-21 21:53:49 +1100

    reformat babel config

--- frontends/station/.babelrc.js
+++ frontends/station/.babelrc.js
@@ -1,15 +1,9 @@
 const modulePlugin = [require.resolve('babel-plugin-module-resolver'), { root: './src/' }];
 
 module.exports = {
+  presets: ['next/babel'],
+  plugins: [modulePlugin],
   env: {
-    development: {
-      presets: ['next/babel'],
-      plugins: [modulePlugin]
-    },
-    production: {
-      presets: ['next/babel'],
-      plugins: [modulePlugin]
-    },
     test: {
       presets: ['@babel/preset-env', '@babel/preset-react', 'next/babel'],
       plugins: [modulePlugin]
