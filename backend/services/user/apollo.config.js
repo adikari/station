aac1527df99c8ea23e8e5afcb700e5e208e7f606
subash adhikari <me@subash.com.au>
2019-12-06 23:51:50 +1100

    push schema to apollo from introspection

--- backend/services/user/apollo.config.js
+++ backend/services/user/apollo.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   service: {
-    localSchemaFile: './schema.graphql'
+    endpoint: {
+      url: process.env.GRAPHQL_ENDPOINT,
+      headers: {
+        'x-api-key': process.env.SERVICE_API_KEY
+      }
+    }
   }
 };
--- backend/services/user/src/config/index.js
+++ backend/services/user/src/config/index.js
@@ -1,4 +1,8 @@
-const { USER_TABLE_NAME } = process.env;
-const { USER_TABLE_EMAIL_INDEX } = process.env;
+const {
+  STAGE,
+  ENGINE_API_KEY,
+  USER_TABLE_NAME,
+  USER_TABLE_EMAIL_INDEX
+} = process.env;
 
-export { USER_TABLE_NAME, USER_TABLE_EMAIL_INDEX };
+export { STAGE, ENGINE_API_KEY, USER_TABLE_NAME, USER_TABLE_EMAIL_INDEX };
